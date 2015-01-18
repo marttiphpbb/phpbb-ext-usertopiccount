80e4a66827d85e44cb14bb4f6da5ab8abbd29c31
martti <info@martti.be>
2015-01-18 23:52:54 +0100

    rm display none from viewtopic and ucp pm

--- styles/prosilver/template/ucp_pm.js
+++ styles/prosilver/template/ucp_pm.js
@@ -1,6 +1,5 @@
 ;(function($, window, document) {
 	$('dl.postprofile dd.profile-topics').each(function(){
 		$(this).insertBefore($(this).siblings('dd.profile-posts'));
-		$(this).show();
 	});
 })(jQuery, window, document);
