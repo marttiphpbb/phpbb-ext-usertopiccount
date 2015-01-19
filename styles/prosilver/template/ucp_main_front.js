dcfc2c60e68f253de97200ae8ffbad8465888629
martti <info@martti.be>
2015-01-19 07:00:04 +0100

    hidden class

--- styles/prosilver/template/ucp_main_front.js
+++ styles/prosilver/template/ucp_main_front.js
@@ -1,3 +1,3 @@
 ;(function($, window, document) {
-	$('div.panel>div.inner>dl.details>dd:eq(1)').after($('div#usertopiccount-hidden').contents());
+	$('div.panel>div.inner>dl.details>dd:eq(1)').after($('div.usertopiccount-hidden').contents());
 })(jQuery, window, document);
--- styles/prosilver/template/ucp_pm.js
+++ styles/prosilver/template/ucp_pm.js
@@ -1,5 +1,6 @@
 ;(function($, window, document) {
 	$('dl.postprofile dd.profile-topics').each(function(){
 		$(this).insertBefore($(this).siblings('dd.profile-posts'));
+		$(this).removeClass('usertopiccount-hidden');
 	});
 })(jQuery, window, document);
