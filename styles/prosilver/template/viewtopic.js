49a80e02ae004844cc9f0c78e0a59ada359efad0
martti <info@martti.be>
2015-06-16 19:37:47 +0200

    fix #3 repeat topic count w thanks for posts

--- styles/prosilver/template/viewtopic.js
+++ styles/prosilver/template/viewtopic.js
@@ -1,6 +1,6 @@
 ;(function($, window, document) {
 	$('dl.postprofile dd.profile-topics').each(function(){
-		$(this).insertBefore($(this).siblings('dd.profile-posts'));
+		$(this).insertBefore($(this).siblings('dd.profile-posts').eq(0));
 		$(this).removeClass('usertopiccount-hidden');
 	});
 })(jQuery, window, document);
